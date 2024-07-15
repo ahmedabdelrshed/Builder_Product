449653f68c83a9af3afdc1380653aeae5040e591
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2024-07-15 15:15:51 +0300

    Add Validation to Inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import ProductCard from "./components/ProductCard";
 import Modal from "./components/ui/Modal";
 import Button from "./components/ui/Button";
 import { formInput } from "./data";
 import Input from "./components/ui/Input";
 import { IProduct } from "./interfaces";
+import { productValidation } from "./validation";
+import ErrorMsg from "./components/ErrorMsg";
 
 function App() {
-  // ---------- STATE ----------
-  const [isOpen, setIsOpen] = useState(false);
-  const [product, setProduct] = useState<IProduct>({
+  const defaultProduct = {
     title: "",
     description: "",
     imageURL: "",
@@ -19,6 +19,17 @@ function App() {
       imageURL: "",
       name: "",
     },
+  };
+  // ---------- STATE ----------
+  const [isOpen, setIsOpen] = useState(false);
+  const [product, setProduct] = useState<IProduct>({
+    ...defaultProduct,
+  });
+  const [error, setError] = useState({
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
   });
   // ------------ HANDLER ---------
   function closeModal() {
@@ -30,12 +41,30 @@ function App() {
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.target;
     setProduct({ ...product, [name]: value });
+    setError({ ...error, [name]: "" });
   };
-  console.log(product);
-
+  const onCancel = () => {
+    console.log("Cancelled");
+    setProduct(defaultProduct);
+    closeModal();
+  };
+  const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const { title, description, price, imageURL } = product;
+    const errors = productValidation({ title, description, price, imageURL });
+    console.log(errors);
+    const checkErrorMsg =
+      Object.values(errors).some((err) => err === "") &&
+      Object.values(errors).every((err) => err === "");
+    if (!checkErrorMsg) {
+      setError(errors);
+      return;
+    }
+    console.log("Send Data Success");
+  };
   // ---------- RENDER -----------
   const renderFormInputs = formInput.map((input) => (
-    <div className="flex flex-col ">
+    <div className="flex flex-col" key={input.id}>
       <label
         htmlFor={input.id}
         className="mb-2 font-medium text-sm text-gray-700"
@@ -49,8 +78,10 @@ function App() {
         value={product[input.name]}
         onChange={onChangeHandler}
       />
+      <ErrorMsg msg={error[input.name]} />
     </div>
   ));
+
   return (
     <main className="container ">
       <div className="flex justify-between items-center  p-2 m-5">
@@ -70,16 +101,13 @@ function App() {
         <ProductCard />
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="Add New Product">
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={submitHandler}>
           {renderFormInputs}
           <div className="flex items-center space-x-3">
             <Button className="bg-indigo-600 hover:bg-indigo-800">
               Submit
             </Button>
-            <Button
-              className="bg-red-600 hover:bg-red-800"
-              onClick={closeModal}
-            >
+            <Button className="bg-red-600 hover:bg-red-800" onClick={onCancel}>
               Cancel
             </Button>
           </div>
---
+++ src/validation/index.ts
@@ -0,0 +1,39 @@
+// ** productObj === validationObj (TITLE,DESCRIPTION,IMAGE,PRICE)
+
+export const productValidation = (product: {
+    title: string, description: string, imageURL: string;
+
+    price: string;
+}) => {
+    const error: {
+        title: string;
+
+        description: string;
+
+        imageURL: string;
+
+        price: string;
+    } = {
+        title: "",
+        description: "",
+        imageURL: "",
+        price: "",
+    }
+    if (product.title.trim().length < 10 || product.title.trim().length > 80) {
+        error.title = "Title must be between 10 and 80 characters long."
+    }
+    if (product.description.trim().length < 10 || product.description.trim().length > 900) {
+        error.description = "Description must be between 10 and 900 characters long."
+    }
+    // Image URL
+    const imageUrlPattern = /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|\d{1,3}(\.\d{1,3}){3}|\[([a-f\d]{1,4}:){7,7}[a-f\d]{1,4}\]|[a-f\d]{1,4}::[a-f\d]{1,4})((:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?)$/i;
+    if (!imageUrlPattern.test(product.imageURL)) {
+        error.imageURL = "Invalid image URL. Please provide a valid URL ."
+    }
+
+    // validate Price 
+    if (isNaN(Number(product.price))) {
+        error.price = "Invalid price. Please provide a valid Price."
+    }
+    return error
+}
